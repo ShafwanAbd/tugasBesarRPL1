4c32f8e992f2b0a41252561b9dc24872b529a814
ShafwanAbd <89147650+ShafwanAbd@users.noreply.github.com>
2022-11-02 18:00:12 +0700

    Add files via upload

--- signup.php
+++ signup.php
@@ -7,11 +7,11 @@
     <link rel="stylesheet" href="./css/css.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="./js/javascript.js"></script>  
-    <title>Log In</title>
+    <title>Sign Up</title>
 </head>
 <body>
-    <div class="login">
-        <h1>Log In</h1>
+    <div class="signup">
+        <h1>Sign Up</h1>
         <div class="row g-3">
             <div class="col">
                 <h3>Username</h3>
@@ -22,13 +22,37 @@
         </div>
         <div class="row g-3">
             <div class="col">
+                <h3>Full Name</h3>
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Write Your Full Name ...">
+            </div>
+        </div>
+        <div class="row g-3">
+            <div class="col">
+                <h3>Jurusan</h3>
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Write Your Jurusan ...">
+            </div>
+        </div>
+        <div class="row g-3">
+            <div class="col">
                 <h3>Password</h3>
             </div>
             <div class="col">
                 <input type="password" class="form-control" placeholder="Write Your Password ...">
             </div>
         </div>
-        <a href="signup.php">Create an Account</a>
+        <div class="row g-3">
+            <div class="col">
+                <h3>Confirm<br>Password</h3>
+            </div>
+            <div class="col">
+                <input type="password" class="form-control" placeholder="Confirm Your Password ...">
+            </div>
+        </div>
+        <p href="#">Dengan melakukan pendaftaran, saya setuju dengan Kebijakan Privasi, Peraturan Klien dan Syarat & Ketentuan Sribulancer.</p>
         <button type="button" class="btn">Submit</button>
     </div>
 </body>
