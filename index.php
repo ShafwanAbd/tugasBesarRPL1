9a4110ee704daf5b4e1397b3e0acef98c79599f2
ShafwanAbd <89147650+ShafwanAbd@users.noreply.github.com>
2022-11-02 13:58:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <script src="javasript.js"></script>  
+        <title>Home</title>
+    </head>
+
+    <nav class="navbar navbar-expand-lg" id="navbarRPL">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">IniJudulnya</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarMenuRPL">
+            <div class="navbar-nav">
+                <a class="nav-link active" href="#home">Home</a>
+                <a class="nav-link" href="#about">About</a>
+                <a class="nav-link" href="#service">Service</a>
+                <a class="nav-link" href="#contact">Contact</a>
+                <a href="#"><img src="./img/login_icon.png"></a>
+            </div>
+        </div>
+    </nav>
+
+    <body>
+        <div class="menu-opening" id="home">
+            <h3>Kesulitan Mengerjakan Tugas?<br>Tidak Ada Waktu?</h3>
+            <img src="./img/img1.png">
+            <div class="menu-opening-button" href="#">
+                <a href="#">Login Sekarang</a>
+            </div>
+        </div>
+
+        <div class="menu-benefit" id="about">
+            <h3>Kami Punya Solusinya!<br>Serahkan Saja Tugas Anda Pada Kami</h3>
+            <div class="row row-cols-1 row-cols-md-3 g-4 d-flex">
+                <div class="col">
+                    <img src="./img/login_icon.png" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Harga Murah</h5>
+                    </div>
+                </div>
+                <div class="col">
+                    <img src="./img/login_icon.png" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Penjoki Handal</h5>
+                    </div>
+                </div>
+                <div class="col">
+                    <img src="./img/login_icon.png" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Transaksi Aman</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="menu-order" id="service">
+            <h3>Kami Punya Solusinya!<br>Serahkan Saja Tugas Anda Pada Kami</h3>
+        </div>
+        
+        <div class="menu-order" id="contact">
+            <h3>Kami Punya Solusinya!<br>Serahkan Saja Tugas Anda Pada Kami</h3>
+        </div>
+
+        <footer class="menu-closing">
+            <p>Created by Muhammad Shafwan Abdullah<br>and Zulfan Syahidan Alfarra</p>
+        </footer>
+    </body>
+</html>
---
+++ profile.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">    
+</head>
+
+<nav class="navbar navbar-expand-lg" id="navbarRPL">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">IniJudulnya</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbarMenuRPL">
+        <div class="navbar-nav">
+            <a href="#"><img src="./img/search_icon.png"></a>
+            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link" href="#">Tugas</a>
+            <a class="nav-link" href="#">Profile</a>
+            <a href="#"><img src="./img/login_icon.png"></a>
+        </div>
+    </div>
+</nav>
+
+<body>
+    <div class="container">
+    <div class="row">
+        <div class="col-sm">
+        One of three columns
+        </div>
+        <div class="col-sm">
+        One of three columns
+        </div>
+        <div class="col-sm">
+        One of three columns
+        </div>
+    </div>
+    </div>
+</body>
+</html>
